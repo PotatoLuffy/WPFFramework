34106c70216b25b9eb224243c496693033f75fa4
potato <812478541@qq.com>
2021-12-22 22:16:07 +0800

    1. implement notification box 2. optimise the right navigation 3. ensure the contentControl

--- EIPMonitor/MainWindow.xaml.cs
+++ EIPMonitor/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using EIPMonitor.CustomUserControlRepository;
+using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +27,35 @@ namespace EIPMonitor
         public MainWindow()
         {
             InitializeComponent();
+            Task.Factory.StartNew(() => Thread.Sleep(2500)).ContinueWith(t =>
+            {
+                //note you can use the message queue from any thread, but just for the demo here we 
+                //need to get the message queue from the snackbar, so need to be on the dispatcher
+                MainSnackbar.MessageQueue?.Enqueue("Welcome to EIP Monitor");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+
+        private void OnSelectedItemChanged(object sender, DependencyPropertyChangedEventArgs e)=> MainScrollViewer.ScrollToHome();
+
+        private async void logOutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var sampleMessageDialog = new SampleMessageDialog
+            {
+                Message = { Text = ((ButtonBase)sender).Content.ToString() }
+            };
+
+            await DialogHost.Show(sampleMessageDialog, "RootDialog");
+        }
+
+        private async void changePasswordBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var sampleMessageDialog = new SampleMessageDialog
+            {
+                Message = { Text = ((ButtonBase)sender).Content.ToString() }
+            };
+
+            await DialogHost.Show(sampleMessageDialog, "RootDialog");
         }
     }
 }
